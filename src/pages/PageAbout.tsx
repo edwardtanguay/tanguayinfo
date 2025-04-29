1f8701cfc09820e9230edc0cf935633d59a945b8
Edward Tanguay (windows) <edward@tanguay.info>
2025-04-29 18:53:07 +0200

    crit

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -2,7 +2,7 @@ export const PageAbout = () => {
 	return (
 		<div className="md:w-[25rem] w-full">
 			<img src="images/edward.jpg" />
-			<p className="text-center mt-2">professional web developer, passionate learner, have philosophy degree, love languages, run marathons, concerned about society, critical as hell</p>
+			<p className="text-center mt-2">professional web developer, passionate learner, have philosophy degree, love languages, run marathons, concerned about society, critical&nbsp;as&nbsp;hell</p>
 			<h2 className="text-lg mt-3">Connect: </h2>
 			<ul className="list-disc ml-6 mt-2 text-lg">
 				<li> <a className="underline" href="https://www.linkedin.com/in/edward-tanguay">linkedin.com/in/edward-tanguay</a></li>
