b9f7c87a8e9de58c4adee208b21ae55c3c0e2182
Edward Tanguay (windows) <edward@tanguay.info>
2025-04-29 18:11:48 +0200

    about smphon

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -2,13 +2,13 @@ export const PageAbout = () => {
 	return (
 		<>
 			<img src="images/edward.jpg" />
-			<p className="text-center">professional web developer, passionate learner, have philosophy degree, love languages, run marathons, concerned about society, critical as hell</p>
-			<h2 className="text-xl mt-3">Connect: </h2>
-			<ul className="list-disc ml-6 text-sm">
+			<p className="text-center mt-2">professional web developer, passionate learner, have philosophy degree, love languages, run marathons, concerned about society, critical as hell</p>
+			<h2 className="text-lg mt-3">Connect: </h2>
+			<ul className="list-disc ml-6 mt-2 text-lg">
 				<li> <a className="underline" href="https://www.linkedin.com/in/edward-tanguay">linkedin.com/in/edward-tanguay</a></li>
 				<li> <a className="underline" href="https://github.com/edwardtanguay">github.com/edwardtanguay</a></li>
 			</ul>
-			<h2 className="text-sm mt-3 border-t border-slate-600 pt-3">This site:</h2>
+			<h2 className="text-sm border-t border-slate-600 pt-3 mt-6">This site:</h2>
 			<ul className="list-disc ml-6 text-sm">
 				<li>live: <a className="underline" href="https://tanguayinfo.vercel.app">tanguayinfo.vercel.app</a></li>
 				<li>repo: <a className="underline" href="https://github.com/edwardtanguay/tanguayinfo">edwardtanguay/tanguayinfo</a></li>
