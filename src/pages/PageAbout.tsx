116fbf61eebc110352c66e164d5f9c69e5afe800
Edward Tanguay (windows) <edward@tanguay.info>
2025-04-29 13:08:39 +0200

    about links

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -1,5 +1,10 @@
 export const PageAbout = () => {
 	return (
-		<p>This is the about page.</p>
+		<>
+			<ul>
+				<li>live: <a className="underline" href="https://tanguayinfo.vercel.app">tanguayinfo.vercel.app</a></li>
+				<li>repo: <a className="underline" href="https://github.com/edwardtanguay/tanguayinfo">edwardtanguay/tanguayinfo</a></li>
+			</ul>
+		</>
 	)
 }
