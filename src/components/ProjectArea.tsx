0324b51bd1df84d815d5a309328bb120613eb225
Edward Tanguay (windows) <edward@tanguay.info>
2025-04-29 17:41:48 +0200

    links

--- src/components/ProjectArea.tsx
+++ src/components/ProjectArea.tsx
@@ -10,10 +10,12 @@ export const ProjectArea = () => {
 				return (
 					<div className="mb-4 w-full" key={index}>
 						<h2 className="mb-1 text-xl text-center font-semibold">{project.title}</h2>
-						<img
-							src={`images/${project.idCode}.png`}
-							className="mb-1 w-full md:w-[30rem] border-[.2rem] rounded-lg border-yellow-500"
-						/>
+						<a href={project.url} target="_blank" rel="noopener noreferrer">
+							<img
+								src={`images/${project.idCode}.png`}
+								className="mb-1 w-full md:w-[30rem] border-[.2rem] rounded-lg border-yellow-500"
+							/>
+						</a>
 						<p className="italic text- text-xs text-center text-slate-300">{project.body}</p>
 					</div>
 				);
