9dcd0ecc278ceb58289a6263af46b6be15d582ab
Edward Tanguay (windows) <edward@tanguay.info>
2025-04-29 15:58:27 +0200

    got lines

--- scripts/classes/DpodFile.ts
+++ scripts/classes/DpodFile.ts
@@ -1,12 +1,16 @@
+import * as qfil from "../qtools/qfil";
 //dpod
 export class DpodFile {
 	private pathAndFileName: string;
-	
+	private lines: string[] = []; 
+
 	constructor(pathAndFileName: string) {
 		this.pathAndFileName = pathAndFileName;
+		this.lines = qfil.getLinesFromFile(this.pathAndFileName);
 	}
 
 	public debug() {
 		console.log("pathAndFileName: " + this.pathAndFileName);
+		console.log("lines: " + this.lines.length);
 	}
 }
