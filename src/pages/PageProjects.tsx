7d81ec939bbe5dc0d89d5a84269f17238a4058d4
Edward Tanguay (windows) <edward@tanguay.info>
2025-04-29 16:51:44 +0200

    showing

--- src/pages/PageProjects.tsx
+++ src/pages/PageProjects.tsx
@@ -1,19 +1,7 @@
+import { ProjectArea } from "../components/ProjectArea";
+
 export const PageProjects = () => {
 	return (
-		<>
-			<p className="mb-3">Click the image to visit:</p>
-			<div className="mb-3">
-				<img
-					src="images/techHowtos.png"
-					className="w-full md:w-[30rem] border-[.3rem] rounded-lg border-gray-500"
-				/>
-			</div>
-			<div className="mb-3">
-				<img
-					src="images/compoutput.png"
-					className="w-full md:w-[30rem] border-[.3rem] rounded-lg border-gray-500"
-				/>
-			</div>
-		</>
+		<ProjectArea />
 	);
 };
