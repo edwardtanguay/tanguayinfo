a83cfd18cab8cbf103a8457022e95284edc419ce
Edward Tanguay (windows) <edward@tanguay.info>
2025-04-29 20:39:49 +0200

    first

--- src/components/ProjectArea.tsx
+++ src/components/ProjectArea.tsx
@@ -9,16 +9,16 @@ export const ProjectArea = () => {
 			{projects.map((project, index) => {
 				return (
 					<div className="mb-4 w-full bg-slate-800 p-3" key={index}>
-						<h2 className="bg-black p-2">
+						<h2 className="bg-slate-950 p-2">
 							<p className="text-xl font-semibold">{project.title}</p>
 						</h2>
 						<a href={project.url} target="_blank" rel="noopener noreferrer">
 							<img
 								src={`images/${project.idCode}.png`}
-								className="w-full md:w-[30rem] border-y-2 border-slate-200"
+								className="w-full md:w-[30rem] border border-slate-950"
 							/>
 						</a>
-						<p className="bg-black p-2 italic text- text-xs text-center text-slate-300">{project.body}</p>
+						<p className="bg-slate-950 p-2 italic text- text-xs text-center text-slate-300">{project.body}</p>
 					</div>
 				);
 			})}
