e2617670feb63814510dff60b20cc5b8853c3695
Edward Tanguay (windows) <edward@tanguay.info>
2025-04-29 17:38:00 +0200

    fix respon

--- src/components/ProjectArea.tsx
+++ src/components/ProjectArea.tsx
@@ -5,10 +5,10 @@ const projects = _projects as Project[];
 
 export const ProjectArea = () => {
 	return (
-		<div>
+		<div className="md:w-[30rem] w-full">
 			{projects.map((project, index) => {
 				return (
-					<div className="mb-4 w-full md:w-[35rem]" key={index}>
+					<div className="mb-4 w-full" key={index}>
 						<h2 className="mb-1 text-xl text-center font-semibold">{project.title}</h2>
 						<img
 							src={`images/${project.idCode}.png`}
